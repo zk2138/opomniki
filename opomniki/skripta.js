4091a22ef8af4fa2ed66f505b09da7946b0c6d4d
zk2138 <zk2138@student.uni-lj.si>
2016-03-08 11:19:02 +0000

    Stran pripravljena za produkcijo

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -5,14 +5,22 @@ window.addEventListener('load', function() {
 	var posodobiOpomnike = function() {
 		var opomniki = document.querySelectorAll(".opomnik");
 		
-		for (i = 0; i < opomniki.length; i++) {
+		for (var i = 0; i < opomniki.length; i++) {
 			var opomnik = opomniki[i];
 			var casovnik = opomnik.querySelector("span");
 			var cas = parseInt(casovnik.innerHTML);
+			var naziv = opomnik.querySelector(".naziv_opomnika").innerHTML;
 	
 			//TODO: 
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
+			if(cas === 0) {
+				alert("Opomnik!\n\nZadolžitev '" + naziv + "' je potekla!");
+				document.querySelector("#opomniki").removeChild(opomnik);
+			} else {
+				cas--;
+				opomnik.querySelector("span").innerHTML = cas;
+			}
 		}
 	}
 	setInterval(posodobiOpomnike, 1000);
